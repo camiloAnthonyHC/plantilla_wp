0f9c54d7d1e416104d8e7cf78af1790d0b1ead5a
camiloAnthonyHC <167445983+camiloAnthonyHC@users.noreply.github.com>
2024-05-09 11:36:01 -0500

    actualizando

---
+++ admin/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title> Xeloro - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MyraStudio" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+            <div class="header-border"></div>
+
+            <?php include ("include/menu.php") ?>
+
+
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-flex align-items-center justify-content-between">
+                                    <h4 class="mb-0 font-size-18">Starter</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                            <li class="breadcrumb-item active">Starter</li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+                        <div class="col-xl-12">
+                            <div class="card">
+                                <div class="card-body">
+
+                                    <h4 class="card-title">With Captions</h4>
+                                    <p class="card-subtitle mb-4">Add captions to your slides easily with the
+                                        <code>.carousel-caption</code> element within any <code>.carousel-item</code>.
+                                    </p>
+
+                                    <div id="carouselExampleCaption" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="carousel-item active">
+                                                <img src="assets/images/media/sm-3.jpg" alt="..."
+                                                    class="d-block img-fluid" width="100%">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h3 class="text-white">First slide label</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="assets/images/media/sm-4.jpg" alt="..."
+                                                    class="d-block img-fluid" width="100%">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h3 class="text-white">Second slide label</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="assets/images/media/sm-5.jpg" alt="..."
+                                                    class="d-block img-fluid"width="100%">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h3 class="text-white">Third slide label</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleCaption" role="button"
+                                            data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleCaption" role="button"
+                                            data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+
+                                </div> <!-- end card-body-->
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+
+               <?php include ("include/footer.php") ?>
+
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Overlay-->
+        <div class="menu-overlay"></div>
+
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/metismenu.min.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/simplebar.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/theme.js"></script>
+
+    </body>
+
+</html>
---
+++ admin/preguntas.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title> Xeloro - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        <div class="header-border"></div>
+        <?php include ("include/menu.php") ?>
+        <!-- Left Sidebar End -->
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0 font-size-18">FAQs</h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                        <li class="breadcrumb-item active">FAQs</li>
+                                    </ol>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>     
+                    <!-- end page title -->
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="text-center mt-4">
+                                <h4>preguntas frecuentes</h4>
+                                <p class="text-muted mt-3 mb-4"> Nisi praesentium similique totam odio obcaecati, reprehenderit,
+                                    dignissimos rem temporibus ea inventore alias!<br/> Beatae animi nemo ea
+                                    tempora, temporibus laborum facilis ut!</p>
+
+                            </div>
+                        </div><!-- end col -->
+                    </div><!-- end row -->
+
+
+                    <div class="row mt-5">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="mb-3 font-weight-bold text-uppercase">General</h5>
+                                    <div id="accordion">
+                                        <div class="card mb-1">
+                                            <div class="card-header bg-white border-bottom-0 p-3" id="headingOne">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                                                        aria-controls="collapseOne" class="text-dark">
+                                                        ¿Cuánto tiempo tardará en llegar mi pedido?                                                    </a>
+                                                </h5>
+                                            </div>
+
+                                            <div id="collapseOne" class="collapse show"
+                                                    aria-labelledby="headingOne" data-parent="#accordion">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card mb-1">
+                                            <div class="card-header bg-white border-bottom-0 p-3" id="headingTwo">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                    ¿Cuál es su política de devolución y reembolso?                                                    </a>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card mb-1">
+                                            <div class="card-header bg-white border-bottom-0 p-3" id="headingThree">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                    ¿Cómo puedo realizar un seguimiento de mi pedido?                                                    </a>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+        
+                                </div>
+                            </div>
+
+
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="mb-3 font-weight-bold text-uppercase">Payments & Support</h5>
+                                    <div id="accordion2">
+                                        <div class="card mb-1">
+                                            <div class="card-header bg-white border-bottom-0 p-3" id="headingFour">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                                        Q. When can be used?
+                                                    </a>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion2">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card mb-1">
+                                            <div class="card-header bg-white border-bottom-0 p-3" role="tab" id="headingFive">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                                        Q. How many variations exist?
+                                                    </a>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion2">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card mb-0">
+                                            <div class="card-header bg-white border-bottom-0 p-3" role="tab" id="headingSix">
+                                                <h5 class="m-0 font-size-16">
+                                                    <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                                        Q. License & Copyright
+                                                    </a>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion2">
+                                                <div class="card-body text-muted pt-1">
+                                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                                                    accusamus terry richardson ad squid. 3 wolf moon officia
+                                                    aute, non cupidatat skateboard dolor brunch. Food truck
+                                                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                                                    sunt aliqua put a bird on it squid single-origin coffee
+                                                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                                                    helvetica, craft beer labore wes anderson cred nesciunt
+                                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                                                    Leggings occaecat craft beer farm-to-table, raw denim
+                                                    aesthetic synth nesciunt you probably haven't heard of them
+                                                    accusamus labore sustainable VHS.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+        
+                                </div>
+                            </div>
+                        </div> <!-- end col -->
+    
+
+                </div> <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            2020 © Xeloro.
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-right d-none d-sm-block">
+                                Design & Develop by Myra
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+    <!-- Overlay-->
+    <div class="menu-overlay"></div>
+
+
+    <!-- jQuery  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/metismenu.min.js"></script>
+    <script src="assets/js/waves.js"></script>
+    <script src="assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/theme.js"></script>
+
+</body>
+
+</html>
